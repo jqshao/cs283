75e8feeec49bb3b4376a8ae8bfb2be05413558e7
cs3834 <cs3834@tux3.cci.drexel.edu>
2025-03-01 18:50:32 -0500

    base A5

---
+++ 5-ShellP3/starter/bats/student_tests.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bats
+
+# File: student_tests.sh
+# 
+# Create your unit tests suit in this file
+
+@test "Example: check ls runs without errors" {
+    run ./dsh <<EOF                
+ls
+EOF
+
+    # Assertions
+    [ "$status" -eq 0 ]
+}
+
+
+# Keep tests from Assignment 3 and 4 so prev functionality works as expected
+
